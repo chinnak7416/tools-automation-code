7a50e45ba072b728397b9b018d17a298280b65ca
RAMBABU <chinnak7416@gmail.com>
2024-03-30 19:13:37 +0530

    Adding instance profile to role

--- modules/main.tf
+++ modules/main.tf
@@ -19,8 +19,6 @@ resource "aws_route53_record" "record" {
 resource "aws_iam_role" "role" {
   name = "${var.tool_name}-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
