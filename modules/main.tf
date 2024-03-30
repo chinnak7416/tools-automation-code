5f96348207876654ea8e361288b179dffe1077b3
RAMBABU <chinnak7416@gmail.com>
2024-03-30 19:36:20 +0530

    Adding instance profile-name to role

--- modules/main.tf
+++ modules/main.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+
 
   tags = {
     Name = var.tool_name
@@ -13,7 +15,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.tool_name}-internal"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instance.private_ip]
+  records = [aws_instance.instance.public_ip]
 }
 
 resource "aws_iam_role" "role" {
