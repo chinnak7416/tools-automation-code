102cfc033a60f3e6de99c00c15790dd4a9c62f14
RAMBABU <chinnak7416@gmail.com>
2024-03-30 18:55:00 +0530

    creating prometheus role

--- modules/main.tf
+++ modules/main.tf
@@ -15,3 +15,27 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
