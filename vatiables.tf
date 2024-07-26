f0e3dee95209c295cb17f4a72edc422ee01d9978
RAMBABU <chinnak7416@gmail.com>
2024-07-26 22:18:05 +0530

    Sonarqube server creating

--- vatiables.tf
+++ vatiables.tf
@@ -4,23 +4,30 @@ variable "tools" {
       instance_type = "t3.small"
       policy_resource_list = ["ec2:DescribeInstances"]
     }
+
     grafana = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
+
     vault = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
+
     elk = {
       instance_type = "r5.large"
       policy_resource_list = []
     }
+
     ci-server = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
-
+    sonarqube = {
+      instance_type = "t3.large"
+      policy_resource_list = []
+    }
   }
 }
 
